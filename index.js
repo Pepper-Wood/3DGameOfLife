063600b25409ab7cc0f8e6f3c90ce488d436f7bf
Darien Keyack <darienkeyack@gmail.com>
2015-12-03 14:19:08 -0500

    Added index.js
    it has only a couple of comments in it basically but I wanted to sync it
    before I left the union.

---
+++ index.js
@@ -0,0 +1,23 @@
+// * SOME JS INITIALIZATION STUFF GOES HERE PROBABLY MAYBE? *
+
+var grid = []; // Grid containing all the cubes
+var gridwidth = 20;
+var gridheight = 20;
+
+//=============================================================================
+// Initialize the grid such that all cubes are false/dead/off
+function initGridClean() {
+ // * DARIEN DOES THIS ONE * //
+}
+
+//=============================================================================
+// Initialize the grid such that cube status is randomized
+function initGridClean() {
+	// * MARINA DOES THIS ONE * //
+}
+
+//=============================================================================
+//=============================================================================
+window.onload = function init() {
+	// * VERY IMPORTANT STUFF GOES HERE YOU SHOULD ADD IT *
+};
