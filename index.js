cfd61890ca6340b2fc5733e26d6eae9516e52f8f
linkknil3 <alexanderjmohr@gmail.com>
2015-12-07 18:34:36 -0500

    PLAYED WITH IT

--- index.js
+++ index.js
@@ -430,10 +430,11 @@ var main = function() {
 				renderer.setSize( window.innerWidth , window.innerHeight );
 				document.body.appendChild( renderer.domElement );
 
-	camera = new THREE.OrthographicCamera(75, window.innerWidth / window.innerHeight, 1, 1000 );
+	// camera = new THREE.OrthographicCamera(75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000 );
 	camera.position.x = 200;
 	camera.position.y = 200;
-	camera.position.z = 200;
+	camera.position.z = 10000;
 
 	// camera.position.x = 500;
 	// camera.position.y = 500;
@@ -479,7 +480,7 @@ function render(type = true) {
 		}
 	}
     
-	var timer = Date.now() * 0.000001;
+	var timer = Date.now() * 0.0001;
 
 	camera.position.x = Math.cos( timer ) * 200;
 	camera.position.z = Math.sin( timer ) * 200;
